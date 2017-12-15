77bc1eb36fa8eb2fb7bd83102033fade633d2635
Fernando Pillo <fernando.pillo@ifood.com.br>
2017-12-15 11:58:00 -0200

    update standard analyzer test

--- src/test/java/com/pillo/mysearchengine/models/StandardAnalyzerTest.java
+++ src/test/java/com/pillo/mysearchengine/models/StandardAnalyzerTest.java
@@ -21,7 +21,7 @@ public class StandardAnalyzerTest {
     }
 
     @Test
-    public void test_analyze() throws Exception {
+    public void test_analyze_should_return_seven_tokens() throws Exception {
         final List<Token> tokens = standardAnalyzer.analyze("This is a test for Standard Analyzer!");
 
         assertEquals(7, tokens.size());
@@ -32,6 +32,22 @@ public class StandardAnalyzerTest {
         assertEquals("for", tokens.get(4).getValue());
         assertEquals("standard", tokens.get(5).getValue());
         assertEquals("analyzer!", tokens.get(6).getValue());
+    }
+
+    @Test
+    public void test_analyze_empty_text_should_return_zero_tokens() throws Exception {
+        final List<Token> tokens = standardAnalyzer.analyze("");
+
+        assertEquals(0, tokens.size());
+    }
+
+    @Test
+    public void test_analyze_duplicated_words_should_return_two_tokens() throws Exception {
+        final List<Token> tokens = standardAnalyzer.analyze(" test test ");
+
+        assertEquals(2, tokens.size());
+        assertEquals("test", tokens.get(0).getValue());
+        assertEquals("test", tokens.get(1).getValue());
     }
 
 }
